d872c86c4e98fc6ec06b4a00b3c7cd7b6ae01dfa
Dan <zimmerman@pliantenergy.com>
2026-05-14 20:31:04 -0400

    Zero x

--- netft_utils/src/netft_utils.cpp
+++ netft_utils/src/netft_utils.cpp
@@ -196,7 +196,7 @@ namespace netft_utils
     // Filter data
     std::vector<double> tempData;
     tempData.resize(6);
-    tempData.at(0) = data->wrench.force.x;
+    tempData.at(0) = 0*data->wrench.force.x;
     tempData.at(1) = data->wrench.force.y;
     tempData.at(2) = data->wrench.force.z;
     tempData.at(3) = data->wrench.torque.x;
